b31985f48d94b1236aaecdc5adae975d399e1d86
Pedro Coelho Torres <pcoelho@coveo.com>
2022-01-26 11:21:34 -0500

    update images

--- base/aem_installer.py
+++ base/aem_installer.py
@@ -5,19 +5,19 @@ from optparse import OptionParser
 # Argument definition
 usage = "usage: %prog [options] arg"
 parser = OptionParser(usage)
-parser.add_option("-i", "--install_file", dest="filename", help="AEM install file")
-parser.add_option("-r", "--runmode", dest="runmode", help="Run mode for the installation")
+parser.add_option("-i", "--install_file", dest="file_name", help="AEM install file")
+parser.add_option("-r", "--runmode", dest="run_mode", help="Run mode for the installation")
 parser.add_option("-p", "--port", dest="port", help="Port for instance")
 
 options, args = parser.parse_args()
 option_dic = vars(options)
-file_name = option_dic.setdefault('filename', 'cq-publish-4503.jar')
-runmode = option_dic.setdefault('runmode', 'publish')
+file_name = option_dic.setdefault('file_name', 'cq-publish-4503.jar')
+run_mode = option_dic.setdefault('run_mode', 'publish')
 port = option_dic.setdefault('port', '4503')
 
 # Copy out parameters
 helpers.log("aem_installer.py called with params: %s" % option_dic)
 
-helpers.import_packages(file_name, port, runmode)
+helpers.import_packages(file_name, port, run_mode)
 
 sys.exit(0)
